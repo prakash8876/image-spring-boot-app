c6c7c18e895d2ca917049d8b02755108f2b1b91d
prakash-b16 <psbhandare16@gmail.com>
2020-06-27 12:55:44 +0530

    uploading base project

---
+++ StudentAngularCRUD/src/main/java/com/prk/main/StudentAngularCrudApplication.java
@@ -0,0 +1,13 @@
+package com.prk.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAngularCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAngularCrudApplication.class, args);
+	}
+
+}
---
+++ StudentAngularCRUD/src/test/java/com/prk/main/StudentAngularCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prk.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAngularCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
